2d53f7398bed926d7b9538faa382b74529b3b4d1
Al Aqmar <alaqmarak0810@gmail.com>
2024-09-24 15:10:20 +0530

    new

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+  params
+}) => {
+  const storeId = params.storeId;
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: storeId,
+    },
+  });
+
+  return(
+  <div>Active Store {store?.name}</div>
+  )
+    
+};
+
+export default DashboardPage;
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { set, useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { redirect } from "next/navigation";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+export const StoreModal = () => {
+  const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/stores", values);
+      console.log(response.data);
+      setLoading(false);
+      toast.success("Store created successfully.");
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong. Please try again.");
+      
+    }
+  };
+  const storeModal = useStoreModal();
+  return (
+    <Modal
+      title={"Create Store"}
+      description={"Add a new store"}
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Temptee"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.name?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
