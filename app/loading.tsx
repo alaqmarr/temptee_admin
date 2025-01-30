6d8f6737f3f88a47527d0f18bbddd00cf6318b99
Al Aqmar <alaqmarak0810@gmail.com>
2025-01-30 14:08:31 +0530

    loading screeen

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div
+            className='w-full flex flex-col items-center justify-center'
+        >
+
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default loading
