0e3064829c2508c9cd23f5a31ed350f2355945fa
Al Aqmar <alaqmarak0810@gmail.com>
2025-02-08 13:19:50 +0530

    payments

---
+++ app/api/payments/razorpay/create-order/route.ts
@@ -0,0 +1,21 @@
+import { RazorpayClient } from "@/lib/payments";
+import { NextResponse } from "next/server";
+
+export default async function GET(req: Request) {
+  const razorpay = RazorpayClient;
+
+  if (!razorpay) {
+    return NextResponse.json(
+      { error: "Razorpay client not found" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "Order created successfully",
+    },
+    { status: 200 }
+  );
+}
